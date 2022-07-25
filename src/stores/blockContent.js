fe08713ed29460cb0a62136e54f2a474c8b23a35
Natcel <natcelnieves@gmail.com>
2022-07-24 23:10:01 -0400

    Store

---
+++ src/stores/blockContent.js
@@ -0,0 +1,13 @@
+import { supabase } from "../supabase";
+import { writable } from "svelte/store";
+export const MainContent = writable([]);
+export let toggle = writable('opened');
+
+export const getContent = async () => {
+    const {data, error} = await supabase.from("BlogContent").select().order('id', {ascending: true});
+    if (error) {
+      console.log(error);
+    }
+    console.log(data);
+    MainContent.set(data);
+  }
