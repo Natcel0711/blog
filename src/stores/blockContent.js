1700a99a530299fd032284e8fd79ae3039f934a6
Natcel <natcelnieves@gmail.com>
2022-07-27 08:46:04 -0400

    side bar and nav bar development on the store

--- src/stores/blockContent.js
+++ src/stores/blockContent.js
@@ -2,6 +2,8 @@ import { supabase } from "../supabase";
 import { writable } from "svelte/store";
 export const MainContent = writable([]);
 export let toggle = writable('opened');
+export let sidebarItem = writable('Dashboard');
+export let routeSelected = writable('Home');
 
 export const getContent = async () => {
     const {data, error} = await supabase.from("BlogContent").select().order('id', {ascending: true});
