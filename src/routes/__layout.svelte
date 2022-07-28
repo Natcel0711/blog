48952acc22f2ed26f2532a283b7d32ae07b532db
Natcel <natcelnieves@gmail.com>
2022-07-28 00:09:05 -0400

    Layout development

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,14 @@
 <script>
     import "../app.css";
     import Nav from "../components/nav.svelte";
+    import Footer from "../components/Footer.svelte"
+    import {supabase} from "../supabase"
+    import SideBar from "../components/SideBar.svelte";
+    console.log(supabase);
   </script>
   <Nav/>
   <main>
+    <SideBar/>
     <slot />
   </main>
+  <Footer/>
