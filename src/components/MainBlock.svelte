03e9df0f104fddb1c478a7e7808cc8a36040e25a
Natcel <natcelnieves@gmail.com>
2022-07-29 12:20:12 -0400

    Adding main Block

---
+++ src/components/MainBlock.svelte
@@ -0,0 +1,18 @@
+<script>
+    import BlockCard from "./BlockCard.svelte";
+    import {getContent, MainContent} from "../stores/blockContent";
+    import DailyBlog from "../components/DailyBlog.svelte"
+    getContent();
+</script>
+<section class="text-gray-600 body-font overflow-hidden">
+    <div class="container px-5 py-24 mx-auto">
+      <div class="-my-4 divide-y-2 divide-gray-100">
+        <DailyBlog/>
+      </div>
+      <div class="-my-8 divide-y-2 divide-gray-100">
+        {#each $MainContent as content }
+            <BlockCard Content={content}/>
+        {/each}
+      </div>
+    </div>
+  </section>
