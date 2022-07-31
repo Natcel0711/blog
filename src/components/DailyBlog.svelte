d021982f1992f6faac59dc5f26a2ef6eac10e099
Natcel <natcelnieves@gmail.com>
2022-07-31 11:54:26 -0400

    Daily blog stuff

--- src/components/DailyBlog.svelte
+++ src/components/DailyBlog.svelte
@@ -1,21 +1,17 @@
 <script>
 	let date = new Date().toLocaleDateString('de-DE');
+	import {getContent} from "../stores/dailyBlogStore"
+	import DailyCard from "../components/DailyBlogCard.svelte"
 </script>
 
 <section class="text-gray-600 body-font">
-	<div class="container mx-auto flex px-5 pb-4 items-center justify-center flex-col">
-		<img
-			style="width: 80%;"
-			class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded image"
-			alt="hero"
-			src="src\images\pexels-pixabay-2156.jpg"
-		/>
-		<div class="middle">
-			<div style="font-size: xx-large;" class="text">
-				<a href="www.google.com">Learn more...</a>
-			</div>
-		</div>
-	</div>
+	{#await getContent() }
+	<DailyCard Title={"Loading"} url="/"/>
+	{:then topPost }
+	<DailyCard Title={topPost.title} url={topPost.url}/>
+	{:catch name }
+	<p style="color: red">Error</p>
+	{/await}
 	<div class="text-center w-full">
 		<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
 			Daily Blog | {date}
@@ -27,36 +23,3 @@
 		<div class="flex justify-center" />
 	</div>
 </section>
-
-<style>
-	.image {
-		opacity: 1;
-		transition: 0.5s ease;
-		backface-visibility: hidden;
-		height: 40em;
-		width: 70em;
-	}
-	.middle {
-		transition: 0.5s ease;
-		opacity: 0;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		-ms-transform: translate(-50%, -50%);
-		text-align: center;
-	}
-	.container:hover .image {
-		opacity: 0.3;
-	}
-	.container:hover .middle {
-		opacity: 1;
-	}
-	.text {
-		border-radius: 25px;
-		font-weight: bold;
-		color: rgb(29, 29, 29);
-		font-size: 16px;
-		padding: 16px 32px;
-	}
-</style>
---
+++ src/components/DailyBlogCard.svelte
@@ -0,0 +1,49 @@
+<script>
+    export let Title = '';
+    export let url = '';
+</script>
+<div class="container mx-auto flex px-5 pb-4 items-center justify-center flex-col">
+    <img
+        style="width: 80%;"
+        class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded image"
+        alt="hero"
+        src="src\images\pexels-pixabay-2156.jpg"
+    />
+    <div class="middle">
+        <div style="font-size: xx-large;" class="text">
+            <a href={url}>{Title}</a>
+        </div>
+    </div>
+</div>
+<style>
+	.image {
+		opacity: 1;
+		transition: 0.5s ease;
+		backface-visibility: hidden;
+		height: 40em;
+		width: 70em;
+	}
+	.middle {
+		transition: 0.5s ease;
+		opacity: 0;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		-ms-transform: translate(-50%, -50%);
+		text-align: center;
+	}
+	.container:hover .image {
+		opacity: 0.3;
+	}
+	.container:hover .middle {
+		opacity: 1;
+	}
+	.text {
+		border-radius: 25px;
+		font-weight: bold;
+		color: rgb(29, 29, 29);
+		font-size: 16px;
+		padding: 16px 32px;
+	}
+</style>
